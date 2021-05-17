605d315836aee425f7f2be01d8aacc42b116a28e
Ben Sinnott <Ben.Sinnott@bridgeheadsoftware.com>
2021-05-17 18:37:26 +0100

    Added handler.

--- MyHandlers/MyHandler.cs
+++ MyHandlers/MyHandler.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
+using MyMessages;
+using Rebus.Handlers;
 
 namespace MyHandlers
 {
-    public class MyHandler
+    public class MyHandler : IHandleMessages<MyMessage>
     {
+        public async Task Handle(MyMessage message)
+        {
+            Console.WriteLine("Handled!");
+        }
     }
 }
